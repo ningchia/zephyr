15c5b7d532e2d2ca9dffad3bed4b146d376e5c0e
lcchen <lcchen@nuvoton.com>
2024-04-23 18:17:37 +0800

    Add runner settings for "jlink" and "openocd" to board "numaker_pfm_m467"

--- boards/arm/numaker_pfm_m467/board.cmake
+++ boards/arm/numaker_pfm_m467/board.cmake
@@ -3,7 +3,15 @@
 board_runner_args(nulink "-f")
 board_runner_args(pyocd "--target=m467hjhae")
 
+# @LC@
+board_runner_args(openocd "--use-elf" "--config=interface/nulink.cfg" "--config=target/numicroM4.cfg")
+board_runner_args(jlink "--device=m467hjhan" "--speed=4000")
+
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nulink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/canopen.board.cmake)
+
+# @LC@
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+
